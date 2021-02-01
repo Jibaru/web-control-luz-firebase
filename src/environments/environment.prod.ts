263c24c1d9a5d3035ecf3bb6c27870227ed1a9e9
jibaru <ignacioruedaboada@hotmail.com>
2021-02-01 12:45:24 -0500

    Toastr & prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDsRBMat6wT3AFm9gdvXTzD7mCVWS214Ao',
+    authDomain: 'iot-sd-arduino.firebaseapp.com',
+    databaseURL: 'https://iot-sd-arduino.firebaseio.com',
+    projectId: 'iot-sd-arduino',
+    storageBucket: 'iot-sd-arduino.appspot.com',
+    messagingSenderId: '599165934747',
+    appId: '1:599165934747:web:5c9c9f9a5e42610e76fa3e',
+  },
 };
